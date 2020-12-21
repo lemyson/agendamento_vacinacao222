b4d6a01ab65af9f021f4b03238a130873a8f02f2
lemyson <lemyson.lemos1001@gmail.com>
2020-12-21 19:01:36 -0300

    agendamento

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agendamento_vacinacao222.settings.base')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "agendamento_vacinacao222.settings.base")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,7 +15,11 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line()
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
 
 
 if __name__ == '__main__':
